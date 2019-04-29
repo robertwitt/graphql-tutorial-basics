047b244574040d3df5f322b20082ad033bd8feb0
Robert Witt <robert.witt@zalando.de>
2019-04-29 16:41:19 +0200

    Add subscriptions for updating and deleting comments.

--- src/resolvers/Mutation.js
+++ src/resolvers/Mutation.js
@@ -164,20 +164,32 @@ const Mutation = {
         }
         db.comments.push(comment)
         
-        pubsub.publish(`comment ${args.data.post}`, { comment })
+        pubsub.publish(`comment ${args.data.post}`, { 
+            comment: {
+                mutation: 'CREATED',
+                data: comment
+            }
+         })
 
         return comment
     },
-    deleteComment(parent, args, { db }, info) {
+    deleteComment(parent, args, { db, pubsub }, info) {
         const commentIndex = db.comments.findIndex(comment => comment.id === args.id)
         if (commentIndex === -1) {
             throw new Error('Comment not found')
         }
-        const deletedComments = db.comments.splice(commentIndex, 1)
+        const [comment] = db.comments.splice(commentIndex, 1)
 
-        return deletedComments[0]
+        pubsub.publish(`comment ${comment.post}`, {
+            comment: {
+                mutation: 'DELETED',
+                data: comment
+            }
+        })
+
+        return comment
     },
-    updateComment(parent, { id, data }, { db }, info) {
+    updateComment(parent, { id, data }, { db, pubsub }, info) {
         const comment = db.comments.find(comment => comment.id === id)
         if (!comment) {
             throw new Error('Comment not found')
@@ -186,6 +198,13 @@ const Mutation = {
         if (typeof data.text === 'string') {
             comment.text = data.text
         }
+
+        pubsub.publish(`comment ${comment.post}`, {
+            comment: {
+                mutation: 'UPDATED',
+                data: comment
+            }
+        })
 
         return comment
     }
